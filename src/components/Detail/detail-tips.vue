1dd1888ee3600d51ec35695a7560e0c46ebd491e
Abdullah Mara <zull.mara00@gmail.com>
2017-05-14 14:33:16 +0300

    Update Tips

--- src/components/Detail/detail-tips.vue
+++ src/components/Detail/detail-tips.vue
@@ -2,11 +2,12 @@
   <div class="tips">
     <span class="title">TIPS</span>
     <ul>
-      <li v-for="i in data.groups[0]">
-        <div class="avatar"></div>
+      <li v-for="item in list">
+        <div class="avatar" :style="{ 'background-image': `url(${avatar(item)})` }">
+        </div>
         <div class="comment">
-          <span>Abdullah Mara</span>
-          <p>Lorem ipsum dolor sit.</p>
+          <span>{{fullName(item)}}</span>
+          <p>{{item.text}}</p>
         </div>
       </li>
     </ul>
@@ -18,5 +19,19 @@
 export default {
   props: ['data'],
   name: 'detail-tips',
+  computed: {
+    list() {
+      return this.data.groups.filter(g => g.type === 'others')[0].items;
+    },
+  },
+  methods: {
+    fullName(item) {
+      return `${item.user.firstName} ${item.user.lastName}`;
+    },
+    avatar(item) {
+      const { photo } = item.user;
+      return `${photo.prefix}40x40${photo.suffix}`;
+    },
+  },
 };
 </script>
